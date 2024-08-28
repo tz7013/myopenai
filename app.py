d12f681f8db55fab752834b614f544dc682da6d5
tz7013 <tz7013@gmail.com>
2024-08-28 14:35:27 +0800

    20240828

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_file
 import myopenai
 
 # 初始化
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 
 # 測試, 在網址後面加上/v1/hello, 網頁顯示'Hello Flask'
 @app.route('/v1/hello')
@@ -24,4 +24,4 @@ def home():
     return send_file('templates/index.html')
 
 # 啟動flask server
-app.run(debug=True, port=80)    # 預設port=80, 在網址後面就不用加":port"
+app.run(debug=True, port=5000)    # 預設port=80, 在網址後面就不用加":port"
